28ec17aecd0f8d0161d92acbe5c5bf0ee1bea0e8
David Barroso <dbarrosop@dravetech.com>
2017-10-16 21:23:07 +0200

    run bird processes in the background

--- bird/run.sh
+++ bird/run.sh
@@ -3,11 +3,11 @@ BIRD_CONF="/bird/bird.conf"
 BIRD6_CONF="/bird/bird6.conf"
 
 if [ -z $BIRD_CONF ]; then
-	bird -c $BIRD_CONF
+	bird -c $BIRD_CONF &
 fi
 
 if [ -z $BIRD6_CONF ]; then
-	bird6 -c $BIRD6_CONF
+	bird6 -c $BIRD6_CONF &
 fi
 
 sleep infinity
