4fd83c2e5673c701477bd1284b851093dbaffb3a
Joao Taveira Araújo <joao.taveira@gmail.com>
2017-10-23 16:57:20 -0700

    Update bird container.

--- bird/run.sh
+++ bird/run.sh
@@ -1,13 +1,3 @@
-#!/bin/sh
-BIRD_CONF="/bird/bird.conf"
-BIRD6_CONF="/bird/bird6.conf"
-
-if [ -z $BIRD_CONF ]; then
-	bird -c $BIRD_CONF &
-fi
-
-if [ -z $BIRD6_CONF ]; then
-	bird6 -c $BIRD6_CONF &
-fi
-
-sleep infinity
+#!/bin/bash
+source /root/config
+bird -f -c bird.conf
